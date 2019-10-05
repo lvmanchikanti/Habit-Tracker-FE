b8db02ef93b194238e751de238dbf49b7c9e6a36
Shuting Mo <cynthia.mo.1812@gmail.com>
2019-10-05 16:27:48 -0400

    update form

--- src/components/CreateGroup/CreateGroup.js
+++ src/components/CreateGroup/CreateGroup.js
@@ -1,21 +1,31 @@
 import React from "react";
-import { Button } from "reactstrap";
+import Button from "@material-ui/core/Button";
+import TextField from "@material-ui/core/TextField";
+import Select from "react-select";
 
-const CreateGroup = ({ onClick }) => {
+const friendOptions = [
+  { value: "cynthia", label: "Cynthia" },
+  { value: "simone", label: "Simone" },
+  { value: "lahari", label: "Lahari" }
+];
+
+const CreateGroup = ({ onClick, setNewGroupCreated }) => {
   return (
-    <React.Fragment>
-      <label>
-        Add to New Collection:
-        <input type="text" name="newCollectionName" />
-      </label>
-      <label>
-        Add Friends:
-        <input type="text" name="friendName" />
-      </label>
-      <Button color="primary" type="button" onClick={onClick}>
+    <>
+      <TextField
+        id="newGroupName"
+        label="Group Name"
+        name="newGroupName"
+        value={""}
+        onChange={() => console.log("test")}
+        margin="normal"
+        variant="outlined"
+      />
+      <Select options={friendOptions} isMulti placeholder="Add Friends" />
+      <Button variant="outlined" color="secondary" onClick={setNewGroupCreated}>
         Create Group
       </Button>
-    </React.Fragment>
+    </>
   );
 };
 
--- src/components/CreateHabit/CreateHabit.js
+++ src/components/CreateHabit/CreateHabit.js
@@ -1,17 +1,37 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Button, Modal } from "reactstrap";
 import CreateGroup from "../CreateGroup";
+import Switch from "@material-ui/core/Switch";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import TextField from "@material-ui/core/TextField";
+import ButtonUI from "@material-ui/core/Button";
+import Select from "react-select";
+
+import {
+  Dropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem
+} from "reactstrap";
 
 const CreateHabit = ({ existingGroups, createNewHabit }) => {
-  const [modalOpen, setModalOpen] = useState(false);
-  const [toggleCreateGroup, setToggleCreateGroup] = useState(false);
+  const [modalOpen, setModalOpen] = useState(true);
+  const [showCreateGroup, setShowCreateGroup] = useState(false);
   const [newHabit, setNewHabit] = useState({
     habitName: "",
     habitGroup: ""
   });
+  const [newGroupCreated, setNewGroupCreated] = useState(false);
 
-  const handleHabitChange = event => {
-    setNewHabit({ ...newHabit, [event.target.name]: event.target.value });
+  const handleHabitChange = (event, name) => {
+    //name change
+    if (event.target) {
+      setNewHabit({ ...newHabit, [event.target.name]: event.target.value });
+    }
+    //dropdown group change
+    else {
+      setNewHabit({ ...newHabit, [name]: event.value });
+    }
   };
   return (
     <>
@@ -39,60 +59,98 @@ const CreateHabit = ({ existingGroups, createNewHabit }) => {
         </div>
         <div className="modal-body">
           <form className="create-habit-modal-form">
-            <label>
-              Habit Name:
-              <input
-                type="text"
-                onChange={event => handleHabitChange(event)}
-                name="habitName"
-              />
-            </label>
-            <Button
-              color="primary"
-              onClick={() => setToggleCreateGroup(!toggleCreateGroup)}
-            >
-              {toggleCreateGroup ? "Cancel" : "Create New Group"}
-            </Button>
-            {!toggleCreateGroup && (
-              <label>
-                Add to Existing Collection:
-                <select
-                  name="habitGroup"
-                  onChange={event => handleHabitChange(event)}
+            <FormControlLabel
+              control={
+                <Switch
+                  checked={showCreateGroup}
+                  onChange={() => setShowCreateGroup(!showCreateGroup)}
+                  value="createNewGroup"
+                />
+              }
+              label="Create New Group"
+            />
+            <TextField
+              id="habitName"
+              label="Habit Name"
+              name="habitName"
+              value={newHabit.habitName}
+              onChange={event => handleHabitChange(event)}
+              margin="normal"
+              variant="outlined"
+            />
+            {/* {!showCreateGroup && (
+              <>
+                <h5>Choose Group</h5>
+                <Dropdown
+                  isOpen={showDropdown}
+                  toggle={() => setShowDropdown(!showDropdown)}
                 >
-                  <option value="">{"Select Existing Habit"}</option>
-                  {existingGroups.map(group => {
-                    return <option value={group}>{group}</option>;
-                  })}
-                </select>
-              </label>
+                  <DropdownToggle caret>
+                    {newHabit.habitGroup ? newHabit.habitGroup : "Select Group"}
+                  </DropdownToggle>
+                  <DropdownMenu>
+                    {existingGroups.map(group => {
+                      return (
+                        <DropdownItem
+                          value={group}
+                          name="habitGroup"
+                          onClick={event => handleHabitChange(event)}
+                        >
+                          {group}
+                        </DropdownItem>
+                      );
+                    })}
+                  </DropdownMenu>
+                </Dropdown>
+              </>
+            )} */}
+            {!showCreateGroup && (
+              <Select
+                defaultValue={newHabit.habitGroup}
+                onChange={event => handleHabitChange(event, "habitGroup")}
+                name="habitGroup"
+                options={existingGroups.map(group => ({
+                  value: group,
+                  label: group
+                }))}
+                placeholder="Choose Group"
+              />
             )}
-            {toggleCreateGroup && (
+            {showCreateGroup && (
               <CreateGroup
-                onClick={() => setToggleCreateGroup(!toggleCreateGroup)}
+                onClick={() => setShowCreateGroup(!showCreateGroup)}
+                setNewGroupCreated={() => setNewGroupCreated(true)}
               />
             )}
           </form>
         </div>
         <div className="modal-footer">
-          <Button
+          <ButtonUI
             color="secondary"
+            variant="outlined"
             data-dismiss="modal"
             type="button"
             onClick={() => setModalOpen(false)}
           >
             Close
-          </Button>
-          <Button
+          </ButtonUI>
+          <ButtonUI
             color="primary"
             type="button"
+            variant="outlined"
             onClick={() => {
               createNewHabit(newHabit);
+              //reset values in newHabit
+              setNewHabit({
+                habitName: "",
+                habitGroup: ""
+              });
               setModalOpen(false);
             }}
+            disabled={showCreateGroup}
           >
             Create Habit
-          </Button>
+          </ButtonUI>
         </div>
       </Modal>
     </>
