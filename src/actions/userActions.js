dcfb115a364b9134f604652355794bb17a27388c
schan2023 <lamschan996@ufl.edu>
2019-11-07 01:33:13 -0500

    displayed all users in landing page

---
+++ src/actions/userActions.js
@@ -0,0 +1,59 @@
+import {
+    CREATE_NEW_USER,
+    GET_EXISTING_USERS,
+    DELETE_USER
+  } from "../constants/actionTypes.js";
+  
+  const usersURL = "http://localhost:8000/users/";
+  
+  /*
+  REDUX ACTION
+  */
+  
+  export const createNewUser = newUser => {
+    // TODO add axios call to POST a new user
+    return {
+      type: CREATE_NEW_USER,
+      payload: newUser
+    };
+  };
+  
+  export const getExistingUsers = existingUsers => {
+    return {
+      type: GET_EXISTING_USERS,
+      payload: existingUsers
+    };
+  };
+  
+  /*
+  API CALLS
+  */
+  
+  // NOTE: May not need now that we fetch in App.js
+  export const getExistingUsersAPI = () => {
+    return async dispatch => {
+      let response = await fetch(usersURL);
+      let data = await response.json();
+  
+      console.log(data);
+  
+      dispatch(getExistingUsers(data));
+    };
+  };
+  
+  export const createNewUserAPI = newUser => {
+    return dispatch => {
+      fetch(usersURL + "register", {
+        headers: { "Content-Type": "application/json" },
+        method: "POST",
+        body: JSON.stringify(newUser)
+      })
+        .then(response => response.json())
+        .then(data => {
+          if (data.name !== "MongoError") {
+            dispatch(createNewUser(data));
+          }
+        });
+    };
+  };
+  
--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -1,9 +1,56 @@
-import React from 'react';
- 
-const Landing  = () => {
-    return (      
-        <h1>Landing Page</h1>
-    );
-}
- 
-export default Landing;
+import React, { useEffect, useState } from "react";
+
+import * as UserActions from "../../actions/userActions.js";
+
+// Import Redux
+import { bindActionCreators } from "redux";
+import { connect } from "react-redux";
+ 
+const Landing = props => {
+    const [users, setUsers] = useState(props.users);
+
+    const usersURL = "http://localhost:8000/users/";
+
+    const fetchUsers = async () => {
+        let response = await fetch(usersURL);
+        let data = await response.json();
+        setUsers(data);
+    };
+
+
+    useEffect(() => {
+        fetchUsers();
+    }, [props.existingUsers]);
+
+    console.log(users)
+
+    return (
+        <div>
+            <h2>Users</h2>
+            {users && users.map(user => {
+                return (
+                  <div key={user._id}>
+                    <h3>{user.name}</h3>
+                  </div>
+                );
+            })}
+        </div>
+    )
+};
+
+const mapStateToProps = state => {
+    return {
+      existingUsers: state.users
+    };
+  };
+  
+  const mapDispatchToProps = dispatch => {
+    return {
+      userActions: bindActionCreators(UserActions, dispatch),
+    };
+  };
+  
+  export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+  )(Landing);
