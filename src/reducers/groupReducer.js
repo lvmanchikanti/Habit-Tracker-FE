f1c6200d6f3c62b9b12f476b2e8e4f5fdd6fa03f
Shuting Mo <42949696+shutingmo@users.noreply.github.com>
2019-11-27 19:42:35 -0500

    Friends dropdown (#8)
    * clean up
    
    * individual habit component
    
    * clean up

--- src/reducers/groupReducer.js
+++ src/reducers/groupReducer.js
@@ -16,15 +16,12 @@ const groupReducer = (state = initialState, action) => {
       };
 
     case "ADD_HABITS_TO_GROUP":
-      //console.log("add habit, state is: ", state);
-      console.log(action.payload);
       let currentGroupsCopy = state.currentGroups.slice();
 
       //use action.payload.groupId to find group in the currentGroups state
       let groupFound = currentGroupsCopy.find(
         ({ _id }) => _id === action.payload.groupId
       );
-      console.log(groupFound);
 
       //if habitObjects array doesn't exist add it to group
       if (!groupFound.habitObjects) {
@@ -32,20 +29,6 @@ const groupReducer = (state = initialState, action) => {
       } else {
         groupFound.habitObjects.push(action.payload.habits);
       }
-
-      // console.log(groupFound);
-
-      // //push new habit in to habit objects array
-      // groupFound.habitObjects = [
-      //   ...groupFound.habitObjects,
-      //   action.payload.habits
-      // ];
-
-      console.log(state);
-
-      // return Object.assign({}, state, {
-      //   currenGroups: currentGroupsCopy
-      // });
 
       return {
         ...state,
