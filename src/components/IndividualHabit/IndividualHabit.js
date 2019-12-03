680453af39b0f4799bb9755a4e5339bb9d87267f
Shuting Mo <cynthia.mo.1812@gmail.com>
2019-12-02 21:32:20 -0500

    progress bar default to 0

--- src/components/IndividualHabit/IndividualHabit.js
+++ src/components/IndividualHabit/IndividualHabit.js
@@ -4,17 +4,19 @@ import IconButton from "@material-ui/core/IconButton";
 import DeleteIcon from "@material-ui/icons/Delete";
 import CheckIcon from "@material-ui/icons/Check";
 
-const IndividualHabit = ({ habitName, progress, deleteHabit }) => {
-  const [progressCounter, setProgressCounter] = useState(progress);
+const IndividualHabit = ({ habitName, deleteHabit }) => {
+  const [progressCounter, setProgressCounter] = useState(0);
 
   return (
     <div>
       <p className="progress-habit-name">{habitName}</p>
 
       <div className="progress-icons">
-        <Progress className="progress-bar-container" value={progressCounter} />
+        <Progress className="progress-bar-container" value={progressCounter}>
+          {progressCounter + "%"}
+        </Progress>
 
-        <IconButton onClick={() => setProgressCounter(progressCounter + 10)}>
+        <IconButton onClick={() => setProgressCounter(progressCounter + 5)}>
           <CheckIcon />
         </IconButton>
         <IconButton aria-label="delete" onClick={deleteHabit}>
